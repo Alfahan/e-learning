4d8274b4f2019cd7eaa3f698ea4dfbb65b8ae70f
Alfahan <ali.farhan160@gmail.com>
2025-02-10 15:45:02 +0700

    feat: integrasi-update-users

--- api-gateway/routes/users.js
+++ api-gateway/routes/users.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const userHandler = require('./handlers/users');
+const veriryToken = require('../middlewares/verifyToken');
 
 router.post('/register', userHandler.register);
 router.post('/login', userHandler.login);
+router.put('/', veriryToken, userHandler.update);
 
 module.exports = router;
