40e6937f6a341180e96976c076e241bc0d8f52d5
Alfahan <ali.farhan160@gmail.com>
2025-02-11 15:11:19 +0700

    feat: integrasi-api-gateway-getUser-logout

--- api-gateway/routes/users.js
+++ api-gateway/routes/users.js
@@ -7,5 +7,7 @@ const veriryToken = require('../middlewares/verifyToken');
 router.post('/register', userHandler.register);
 router.post('/login', userHandler.login);
 router.put('/', veriryToken, userHandler.update);
+router.get('/', veriryToken, userHandler.getUser);
+router.post('/logout', veriryToken, userHandler.logout);
 
 module.exports = router;
