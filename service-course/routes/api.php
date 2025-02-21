ac28c6191f9f64ee3c8428bbf0094605666fc8cb
Alfahan <ali.farhan160@gmail.com>
2025-02-21 22:00:11 +0700

    feat: CRUD Course

--- service-course/routes/api.php
+++ service-course/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CourseController;
 use App\Http\Controllers\MentorController;
 use Illuminate\Support\Facades\Route;
 
@@ -9,3 +10,8 @@ Route::get('mentors/{id}', [MentorController::class, 'show']);
 Route::post('mentors', [MentorController::class, 'create']);
 Route::put('mentors/{id}', [MentorController::class, 'update']);
 Route::delete('mentors/{id}', [MentorController::class, 'destroy']);
+
+Route::get('courses', [CourseController::class, 'index']);
+Route::post('courses', [CourseController::class, 'create']);
+Route::put('courses/{id}', [CourseController::class, 'update']);
+Route::delete('courses/{id}', [CourseController::class, 'destroy']);
