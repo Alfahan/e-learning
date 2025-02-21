abff5cc05b57802548b5ea4f4db124a326395aca
Alfahan <ali.farhan160@gmail.com>
2025-02-21 20:36:28 +0700

    feat: CRUD-mentors

--- service-course/routes/api.php
+++ service-course/routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\MentorController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+Route::get('mentors', [MentorController::class, 'index']);
+Route::get('mentors/{id}', [MentorController::class, 'show']);
+Route::post('mentors', [MentorController::class, 'create']);
+Route::put('mentors/{id}', [MentorController::class, 'update']);
+Route::delete('mentors/{id}', [MentorController::class, 'destroy']);
